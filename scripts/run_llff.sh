a4f3b915947db337d5a8de5b25b2a6218fec8ead
Jeonghwan Cho <120773889+ai-cho@users.noreply.github.com>
2025-02-24 16:20:23 +0900

    Update run_llff.sh

--- scripts/run_llff.sh
+++ scripts/run_llff.sh
@@ -29,7 +29,7 @@ for SCENE in "aloe" "art" "flowers" "garbage" "picnic" "roses" ; do
     workspace=$workspace_base/$SCENE
     dataset=$dataset_base/$SCENE
     echo "training $workspace"
-    python train_llff.py  -s $dataset --model_path $workspace -r 2 --eval --n_sparse 250  --rand_pcd --iterations 6000 --lambda_dssim 0.2 \
+    python train_llff.py  -s $dataset --model_path $workspace -r 2 --eval --n_sparse 3  --rand_pcd --iterations 6000 --lambda_dssim 0.2 \
                 --densify_grad_threshold 0.0013 --prune_threshold 0.01 --densify_until_iter 6000 --percent_dense 0.01 \
                 --position_lr_init 0.016 --position_lr_final 0.00016 --position_lr_max_steps 5500 --position_lr_start 500 \
                 --split_opacity_thresh 0.1 --error_tolerance 0.00025 \
